cbdfc3b6e07c65932fccdb7e0254ced10aad9c83
DSVerster <denzelverster@gmail.com>
2025-08-19 20:57:11 +0200

    Factories and Seeder files

--- database/factories/GroupMemberFactory.php
+++ database/factories/GroupMemberFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\GroupMember>
  */
-class UserFactory extends Factory
+class GroupMemberFactory extends Factory
 {
     /**
      * Define the model's default state.
---
+++ database/seeders/GroupMembersSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GroupMembersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
