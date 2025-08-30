a9116a1894b17b2b504198413bb05d2684edd342
DSVerster <denzelverster@gmail.com>
2025-08-30 20:21:17 +0200

    User CRUD - UserAPIController & UserController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use app\Http\Controllers\UserController;
+use app\Http\Controllers\UserAPIController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -11,6 +13,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::resource('users', UserController::class);
+    Route::apiResource('users', UserAPIController::class);
 });
 
 require __DIR__.'/settings.php';
