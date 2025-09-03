ae81ead03e8e2593cc7ab405e4ae5e1f53fba66b
ClaytonCTW123 <41070402@mynwu.ac.za>
2025-09-03 11:29:25 +0200

    Resolve merge conflicts in routes

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,17 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin' => false, 
-        'canRegister' => false, 
+        'canLogin' => false,
+        'canRegister' => false,
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
 });
 
-<<<<<<< HEAD
 Route::get('/inertia-login', function () {
     return Inertia::render('inertia-login');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
-=======
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
@@ -30,4 +26,5 @@ Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit')
 Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
->>>>>>> 459ddf327a8c43b0e4b152e1532d5adf7c5708ca
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
