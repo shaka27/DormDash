8b1e06fd0c5c1c2805fb7f4bf319d63e9e4bcea8
teejay32 <mlondieteejay@gmail.com>
2025-10-11 18:58:32 +0200

    removed staffcontroller for staff page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\StaffController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
